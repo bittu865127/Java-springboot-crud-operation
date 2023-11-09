37968bd8813f20bdf965aa5dc11d7befc4ef0491
bittu865127 <bittu841208@gmail.com>
2023-11-10 00:11:19 +0530

    Springboot-CRUD-Operation

---
+++ Springboot-CRUD-Operation/src/main/java/com/bittusharma/springboot/controller/EmployeeController.java
@@ -0,0 +1,75 @@
+package com.bittusharma.springboot.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bittusharma.springboot.exception.ResourceNotFoundException;
+import com.bittusharma.springboot.model.Employee;
+import com.bittusharma.springboot.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	// get all employees
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees() {
+		return employeeRepository.findAll();
+	}
+
+	// create employee rest api
+	@PostMapping
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+	// get Employee By Id rest api
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(()->  new ResourceNotFoundException("Employee not exist with id :" + id));
+		return ResponseEntity.ok(employee);
+	}
+
+	// update employee rest api
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id ,@RequestBody Employee employeeDetails) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(()->  new ResourceNotFoundException("Employee not exist with id :" + id));
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setEmailId(employeeDetails.getEmailId());
+		
+		Employee updateEmployee = employeeRepository.save(employee);
+		return ResponseEntity.ok(updateEmployee);
+	}
+	
+	// delete employee rest api
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(()->  new ResourceNotFoundException("Employee not exist with id :" + id));
+		
+		employeeRepository.delete(employee);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
+
+}
---
+++ Springboot-CRUD-Operation/src/test/java/com/bittusharma/springboot/SpringbootCrudOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bittusharma.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
